c717fd6c638061ae0261f3636650cdbf56ef7c11
roris <roris@users.noreply.github.com>
2015-02-05 22:39:52 +0500

    Create mqueue.h

---
+++ mqueue.h
@@ -0,0 +1,28 @@
+#ifndef MQUEUE_H
+#define MQUEUE_H
+
+#include <stddef.h>
+
+/* not defined by windows */
+#define O_NONBLOCK  0x10000000L /* do not wait */
+#define O_NORESERVE 0x20000000L /* commit the memory for the queue */
+#define O_PRIVATE   0x40000000L /* process specific queue */
+
+/* MQ constants */
+#define MQ_OPEN_MAX 128 /* maximum number of open message queues */
+#define MQ_MSG_SIZE 512 /* maximum message size */
+#define MQ_MAX_MSG  256 /* maximum number of messages per queue */
+#define MQ_PRIO_MAX 16  /* number of priorities */
+
+typedef int mqd_t;
+
+struct mq_attr {
+    long mq_flags;
+    long mq_maxmsg;
+    long mq_msgsize;
+    long mq_curmsg;
+};
+
+int mq_send(mqd_t mqdes, const char *msg_ptr, size_t msg_len, unsigned msg_prio);
+
+#endif
